af457a7c58e9f616d841d248129a2631d3bbb8a8
Nguyen Duc Tam <tamgtasa2611@gmail.com>
2024-09-05 15:59:04 +0700

    cors configuration

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'https://portfovio.vercel.app/')],
+    'allowed_origins' => [env('FRONTEND_URL', 'https://portfovio.vercel.app')],
 
     'allowed_origins_patterns' => [],
 
